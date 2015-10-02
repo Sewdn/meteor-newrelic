cf5512673669eed788dc737a082f1fef2fc3929c
Pieter Soudan <pieter.soudan@redandivory.be>
2015-10-02 10:49:46 +0200

    initial setup

---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  summary: "Meteor NewRelic agent",
+  "name": "sewdn:newrelic",
+  "version": "1.22.1",
+  "author": "Sewdn (https://twitter.com/Sewdn)",
+  "homepage": "https://github.com/mycartio/newrelic",
+  "git": "https://github.com/mycartio/newrelic.git"
+});
+
+Npm.depends({
+  "newrelic": "1.22.1"
+});
+
+Package.on_use(function(api) {
+  api.versionsFrom("METEOR@0.9.0");
+
+  if (api.export) {
+    api.export('newrelic', 'server');
+  }
+
+  api.use(['underscore'], 'server');
+
+  api.add_files('lib/server.js', 'server');
+});
